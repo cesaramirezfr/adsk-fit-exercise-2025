bdb6716ff290fc9888ab8f2e04491c182af6052e
Cesar Ramirez <cesaraugusto.ramirez@globant.com>
2025-08-26 00:25:43 -0500

    improve tests

--- src/tests/root.test.ts
+++ src/tests/root.test.ts
@@ -5,6 +5,6 @@ describe("GET /", () => {
   it("should return hello message", async () => {
     const res = await request(app).get("/");
     expect(res.status).toBe(200);
-    expect(res.body).toHaveProperty('message');
+    expect(res.body).toHaveProperty("message");
   });
 });
